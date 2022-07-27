930f21916c5105c7c5aaf91cf739e3c76e35ea30
PillaiManish <pillaimanish00@gmail.com>
2022-07-27 17:47:00 +0530

    OTP Generation

--- services/auth-services.js
+++ services/auth-services.js
@@ -2,7 +2,8 @@ let logHelpers = require('../helpers/log-helper')
 let dbHelpers = require('../helpers/db-helper')
 let crypto = require('crypto'); 
 let mails = require('../helpers/mail-helper').sendMails
-
+let totp = require("totp-generator");
+let base32 = require('base-32')
 
 let register = (name, email)=>{
     return new Promise(async(resolve, reject)=>{
@@ -17,10 +18,11 @@ let register = (name, email)=>{
             return reject({error:err, message: "Could not save the data."})
         }
 
-        
+        let emailBase32 = base32.default.encode(email);
+        let OTP = totp(emailBase32, {period: 270});
 
         try{
-            result = await mails.sendEmailOTP(email,"12345") 
+            result = await mails.sendEmailOTP(email,OTP) 
         }
         catch(err){
             return reject({error:err, message: "Could not send the OTP."})
